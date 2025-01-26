53f8c8067889eb098b250c2ae2dfa5fa921d04a4
Shubham Handore <shubhamps0109@gmail.com>
2025-01-26 18:55:28 +0530

    Updated

---
+++ JavaScript/practice.js
@@ -0,0 +1,21 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+
+    if (num % 2 === 0 || num % 3 === 0) return false;
+
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+
+    return true;
+}
+
+function getPrimeNumbers(arr) {
+    return arr.filter(isPrime);
+}
+
+// Example usage:
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const primeNumbers = getPrimeNumbers(numbers);
+console.log(primeNumbers); // Output: [2, 3, 5, 7, 11, 13]
